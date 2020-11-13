f63e59f874bed27b73b2a46b8fcf9fd3227e6fd9
carlos mur <cmurestudillos@gmail.com>
2020-11-13 08:05:29 +0100

    commit

--- src/components/home/HomeComponent.vue
+++ src/components/home/HomeComponent.vue
@@ -3,8 +3,8 @@
   <div>
     <md-tabs class="md-transparent" md-alignment="fixed">
       <md-tab id="tab-home" :md-label="modenaEur.description"></md-tab>
-      <md-tab id="tab-pages" md-label="Pages"></md-tab>
-      <md-tab id="tab-posts" md-label="Posts"></md-tab>
+      <md-tab id="tab-pages" :md-label="modenaUsd.description"></md-tab>
+      <md-tab id="tab-posts" :md-label="modenaGbp.description"></md-tab>
     </md-tabs>
   </div>
 </template>
@@ -15,18 +15,30 @@
 import axios from 'axios';
 // EndPoint
 import dataEur from '../../api/eur.js';
+import dataUsd from '../../api/usd.js';
+import dataGbp from '../../api/gbp.js';
 
 export default {
     name: 'HomeComponent',
     mounted(){
 
-        // Llamamos al metodo
+        // Llamamos al metodo Euro
         this.getEurCurerency();
+
+        // Llamamos al metodo Dolares
+        this.getUsdCurerency();
+
+        // Llamamos al metodo Libras
+        this.getGbpCurerency();
     },
     data(){
         return {
-            api: dataEur.endpoint,
+            apiEur: dataEur.endpoint,
+            apiUsd: dataUsd.endpoint,
+            apiGbp: dataGbp.endpoint,
             modenaEur: [],
+            modenaUsd: [],
+            modenaGbp: [],
         }
     },  
     methods: {
@@ -37,14 +49,44 @@ export default {
             // Log de seguimiento
             console.log('HomeComponent.vue - Metodo getEurCurerency');
 
-            axios.get(this.api)
+            axios.get(this.apiEur)
                 .then( res => {
                     if(res.data){
                     this.modenaEur = res.data['bpi'].EUR;
                     console.log(this.modenaEur)
-                    }
-                });
-            },
+                }
+            });
+        },
+        //-------------------------------------------------------//
+        // Metodo para obtener datos en Dolares                  //
+        //-------------------------------------------------------//
+        getUsdCurerency(){
+            // Log de seguimiento
+            console.log('HomeComponent.vue - Metodo getUsdCurerency');
+
+            axios.get(this.apiUsd)
+                .then( res => {
+                    if(res.data){
+                    this.modenaUsd = res.data['bpi'].USD;
+                    console.log(this.modenaUsd)
+                }
+            });
+        },
+        //-------------------------------------------------------//
+        // Metodo para obtener datos en Libras                   //
+        //-------------------------------------------------------//
+        getGbpCurerency(){
+            // Log de seguimiento
+            console.log('HomeComponent.vue - Metodo getGbpCurerency');
+
+            axios.get(this.apiGbp)
+                .then( res => {
+                    if(res.data){
+                    this.modenaGbp = res.data['bpi'].GBP;
+                    console.log(this.modenaGbp)
+                }
+            });
+        }                       
     }        
 }
 
